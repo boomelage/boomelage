0df8d8b8d1798364f77d2c505a2b20632c429838
boomelage <163606270+boomelage@users.noreply.github.com>
2025-11-05 17:49:58 +0200

    make axes and text white with black outline

--- gbm_simulator/gbm_simulator.py
+++ gbm_simulator/gbm_simulator.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as pe
 import os
 import shutil
 import imageio.v2 as imageio
@@ -16,12 +17,13 @@ def generate_gbm(N, T, mu, sigma, S0, r):
 def plot_gbm(paths, t, output_dir, T, min_log_return, max_log_return, max_density):
     N = paths.shape[1]
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 6), gridspec_kw={'width_ratios': [3, 1]})
+    outline_effect = [pe.Stroke(linewidth=2, foreground='black'), pe.Normal()]
 
     # Plot paths
     ax1.plot(paths[:t + 1, :], lw=0.5)
-    ax1.set_title(f'Geometric Brownian Motion\n(t={t})')
-    ax1.set_xlabel('Time Steps')
-    ax1.set_ylabel('Price')
+    title1 = ax1.set_title(f'Geometric Brownian Motion\n(t={t})')
+    xlabel1 = ax1.set_xlabel('Time Steps')
+    ylabel1 = ax1.set_ylabel('Price')
     ax1.set_xlim(0, T)
     ax1.set_ylim(0, paths.max())
 
@@ -30,12 +32,31 @@ def plot_gbm(paths, t, output_dir, T, min_log_return, max_log_return, max_densit
         total_log_returns = np.log(paths[t, :] / paths[0, :])
         ax2.hist(total_log_returns, bins=int(np.sqrt(total_log_returns.size)), orientation='horizontal', density=True, color = "green")
 
-    ax2.set_title('Log-Returns\nDistribution')
-    ax2.set_xlabel('Density')
-    ax2.set_ylabel('Log-Returns')
+    title2 = ax2.set_title('Log-Returns\nDistribution')
+    xlabel2 = ax2.set_xlabel('Density')
+    ylabel2 = ax2.set_ylabel('Log-Returns')
     ax2_ylim = max(abs(min_log_return), abs(max_log_return))
     ax2.set_ylim(-ax2_ylim, ax2_ylim)
     ax2.set_xlim(0, max_density * 1.1)
+
+    for ax in (ax1, ax2):
+        ax.tick_params(colors='white')
+        for label in ax.get_xticklabels() + ax.get_yticklabels():
+            label.set_color('white')
+            label.set_path_effects(outline_effect)
+        for spine in ax.spines.values():
+            spine.set_color('white')
+            spine.set_path_effects(outline_effect)
+        ax.xaxis.label.set_color('white')
+        ax.xaxis.label.set_path_effects(outline_effect)
+        ax.yaxis.label.set_color('white')
+        ax.yaxis.label.set_path_effects(outline_effect)
+        ax.title.set_color('white')
+        ax.title.set_path_effects(outline_effect)
+
+    for text in (title1, xlabel1, ylabel1, title2, xlabel2, ylabel2):
+        text.set_color('white')
+        text.set_path_effects(outline_effect)
 
     plt.tight_layout()
     plt.savefig(os.path.join(output_dir, f'gbm_{t:03d}.png'), transparent=True)
